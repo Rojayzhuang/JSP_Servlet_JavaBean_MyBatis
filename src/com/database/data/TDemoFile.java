539ab3cf0d159d313dbae58e6a938d19ecea3709
庄鲁杰 <sx_099129@travelsky.com>
2019-11-29 15:34:19 +0800

    增加了文件表的持久化类与数据库访问的映射接口

--- src/com/database/data/TDemoFile.java
+++ src/com/database/data/TDemoFile.java
@@ -1,11 +1,91 @@
 package com.database.data;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+
 /**
+ * 文件表的持久化类
  * @author Rojay
  * @version 1.0.0
  * @createTime 2019年11月29日  11:26:43
  */
-public class TDemoFile {
+public class TDemoFile implements Serializable {
 
+    private Integer fileId; //文件ID
+    private String fileName; //文件名（逻辑名称--展示给用户看到的文件名）
+    private String fileUrl; //文件路径（物理名称--磁盘中的名称）
+    private String fileType; //文件类型
+    private String fileSuffix; //后缀
+    private Integer fileSize; //文件长度
+    private Timestamp operTime; //操作事件
+
+    public TDemoFile() {
+
+    }
+
+    public TDemoFile(String fileName, String fileUrl, String fileType,
+                     Integer fileSize, Timestamp operTime) {
+        this.fileName = fileName;
+        this.fileUrl = fileUrl;
+        this.fileType = fileType;
+        this.fileSize = fileSize;
+        this.operTime = operTime;
+    }
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileSuffix() {
+        return fileSuffix;
+    }
+
+    public void setFileSuffix(String fileSuffix) {
+        this.fileSuffix = fileSuffix;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Integer fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Timestamp getOperTime() {
+        return operTime;
+    }
+
+    public void setOperTime(Timestamp operTime) {
+        this.operTime = operTime;
+    }
 
 }
--- src/com/mapper/ITDemoFileMapper.java
+++ src/com/mapper/ITDemoFileMapper.java
@@ -1,9 +1,49 @@
 package com.mapper;
 
+import java.util.List;
+
 /**
+ * 数据库访问的映射接口
  * @author Rojay
  * @version 1.0.0
  * @createTime 2019年11月29日  11:26:56
  */
 public interface ITDemoFileMapper {
+
+    /**
+     * 查询所有数据
+     * @return
+     * @throws Exception
+     */
+    public List<TDemoFile> selectAll(String fileName) throws Exception;
+
+    /**
+     * 根据ID获得文件数据
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public TDemoFile getById(Integer id) throws Exception;
+
+    /**
+     * 添加文件数据
+     * @param tdemoFile
+     * @throws Exception
+     */
+    public void insert(TDemoFile tdemoFile) throws Exception;
+
+    /**
+     * 修改文件数据
+     * @param tdemoFile
+     * @throws Exception
+     */
+    public void update(TDemoFile tdemoFile) throws Exception;
+
+    /**
+     * 删除文件数据
+     * @param ids
+     * @throws Exception
+     */
+    public void deleteBatch(String[] ids) throws Exception;
+
 }
